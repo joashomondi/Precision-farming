795ac591eacc0d23543a274e9f649aeb8b6e199f
Kerry Onyeogo <kerryonyeogo@gmail.com>
2023-11-22 11:53:54 +0100

    added requirements.txt

--- PrecisionFarming/settings.py
+++ PrecisionFarming/settings.py
@@ -124,13 +124,13 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     "http://127.0.0.1:8000",
-    "https://hostelcomplaintsmanagementsystem.onrender.com"
+    # hosting domain
 ]
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     "http://127.0.0.1:8000",
-    "https://hostelcomplaintsmanagementsystem.onrender.com"
+    # hosting domain
 ]
 
 CORS_ALLOW_HEADERS = [
--- authentication/models.py
+++ authentication/models.py
@@ -24,7 +24,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     date_joined = models.DateTimeField(default=timezone.now)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
-    is_verified = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=True)
     
     objects = CustomUserManager()
     USERNAME_FIELD = 'email'
@@ -33,9 +33,5 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.first_name + ' ' + self.last_name
     
-    def save(self, *args, **kwargs):
-        if self.is_superuser:
-            self.is_verified = True 
-        return super().save(*args, **kwargs)
     
     
