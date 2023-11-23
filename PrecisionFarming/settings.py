5dd9e8be7a716a7f5bbb4d7998987b9c747db7aa
Kerry Onyeogo <kerryonyeogo@gmail.com>
2023-11-23 15:16:00 +0100

    changed cors settings

--- PrecisionFarming/settings.py
+++ PrecisionFarming/settings.py
@@ -137,13 +137,19 @@ SWAGGER_SETTINGS = {
 # cors settings
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    "http://127.0.0.1:8000",
+    'http://localhost:5173',
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:3000',
+    'http://127.0.0.1:5173',
     # hosting domain
 ]
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
-    "http://127.0.0.1:8000",
+    'http://localhost:5173',
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:3000',
+    'http://127.0.0.1:5173',
     # hosting domain
 ]
 
