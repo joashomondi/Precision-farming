2e7991ce1a48b7414b63823f94e92543f1754dd8
joashomondi <92590542+joashomondi@users.noreply.github.com>
2025-08-29 14:01:12 +0300

    Delete com-sci directory

--- com-sci/authentication/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import permissions 
-
-class CustomUserPermissions(permissions.BasePermission):
-    
-    def has_permission(self, request, view):
-        if request.user.is_verified:
-            return True 
-        return False 
